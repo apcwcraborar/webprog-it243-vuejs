f671059902cd3271eea9a917d357c6a2e3f76eb1
Wenchie Arhowen Raborar <wcraborar@student.apc.edu.ph>
2026-01-26 15:24:00 +0800

    Add view and insert comment/visitor feedback functionality to your website

---
+++ firstsfc/src/components/Comment.vue
@@ -0,0 +1,46 @@
+<template>
+    <h1>Comments</h1>
+    <ul>
+      <li v-for="comment in comments" :key="comment.id">{{ comment.name }} {{ comment.comment }}</li>
+    </ul>
+  </template>
+  
+  <script></script>
+  
+  <style>
+  #app > div {
+    border: dashed rgb(93, 22, 49) 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(255, 178, 220);
+  }
+  </style>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const comments = ref([])
+
+async function getComments() {
+  const { data } = await supabase.from('comments').select()
+  comments.value = data
+}
+
+onMounted(() => {
+  getComments()
+})
+
+</script>
+
+
+<style>
+  #app > div {
+    border: dashed rgb(93, 22, 49) 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(255, 178, 220);
+  }
+</style>
