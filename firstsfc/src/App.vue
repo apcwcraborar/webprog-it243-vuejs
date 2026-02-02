dab8b15c68090996694c41c2d385302f2c305b38
Wenchie Arhowen Raborar <wcraborar@student.apc.edu.ph>
2026-02-02 06:36:05 +0000

    Add REST API calls to your Vue.js website

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -5,7 +5,7 @@ cd <template>
   <food-item/>
   <comment-form/>
   <comment/>
-   
+  <rest-api/>    
 
 </template>
 
--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
+import RestApi from './components/RestApi.vue'
+
 
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
+app.component('rest-api', RestApi)
 app.mount('#app')
-
